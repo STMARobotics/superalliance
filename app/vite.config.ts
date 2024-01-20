d3585e289130d1184b7438d525ca1986c1de4ce2
818523 <74215559+BlakeMoran14@users.noreply.github.com>
2024-01-20 15:20:28 -0600

    another fix

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -11,12 +11,4 @@ export default defineConfig({
     },
   },
   base: "/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "nested/index.html"),
-      },
-    },
-  },
 });
