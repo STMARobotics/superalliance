ddbeb0e1f5456b1fb429f857ee10ba8d3bc80a0d
818523 <74215559+BlakeMoran14@users.noreply.github.com>
2024-01-20 15:15:21 -0600

    update config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path, { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,14 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "nested/index.html"),
+      },
     },
   },
 });
