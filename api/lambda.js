d5e940aa5d1fe9681f2d92681a051904e0938d33
Chad LaVigne <chad.lavigne@solutiondesign.com>
2024-03-03 14:59:16 -0600

    working on connection issue

--- api/lambda.js
+++ api/lambda.js
@@ -14,47 +14,47 @@ const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 const mongoose = require("mongoose");
 let connection = null;
 
-exports.handler = async function(event, context) {
+// exports.handler = async function(event, context) {
 
-  // Make sure to add this so you can re-use `conn` between function calls.
-  // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
-  context.callbackWaitsForEmptyEventLoop = false;
-  
-  // Because `connection` is in the global scope, Lambda may retain it between
-  // function calls thanks to `callbackWaitsForEmptyEventLoop`.
-  // This means your Lambda function doesn't have to go through the
-  // potentially expensive process of connecting to MongoDB every time.
-  
-  if (connection == null) {
-
-    connection = mongoose.connect(process.env.MONGODB_URI, {family: 4, serverSelectionTimeoutMS: 5000})
-      .then(() => {        
-        console.log("Connected to Mongo!");
-        return mongoose;
-      })
-      .catch(error => {
-        console.error("Error connecting to MongoDB:", err);
-      });
-
-    // connection = mongoose
-    // .connect(process.env.MONGODB_URI, { family: 4 })
-    // .then(console.log("Connected to Mongo!"))
-    // .catch(console.error);
-
-    // `await`ing connection after assigning to the `conn` variable
-    // to avoid multiple function calls creating new connections
-    await connection;
-  }
-
-  awsServerlessExpress.proxy(server, event, context);
-}
-
-// exports.handler = (event, context) => {
+//   // Make sure to add this so you can re-use `conn` between function calls.
+//   // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
 //   context.callbackWaitsForEmptyEventLoop = false;
-//   mongoose
-//   .connect(process.env.MONGODB_URI, { family: 4 })
-//   .then(console.log("Connected to Mongo!"))
-//   .catch(console.error);
+  
+//   // Because `connection` is in the global scope, Lambda may retain it between
+//   // function calls thanks to `callbackWaitsForEmptyEventLoop`.
+//   // This means your Lambda function doesn't have to go through the
+//   // potentially expensive process of connecting to MongoDB every time.
+  
+//   if (connection == null) {
+
+//     connection = mongoose.connect(process.env.MONGODB_URI, {family: 4, serverSelectionTimeoutMS: 5000})
+//       .then(() => {        
+//         console.log("Connected to Mongo!");
+//         return mongoose;
+//       })
+//       .catch(error => {
+//         console.error("Error connecting to MongoDB:", err);
+//       });
+
+//     // connection = mongoose
+//     // .connect(process.env.MONGODB_URI, { family: 4 })
+//     // .then(console.log("Connected to Mongo!"))
+//     // .catch(console.error);
+
+//     // `await`ing connection after assigning to the `conn` variable
+//     // to avoid multiple function calls creating new connections
+//     await connection;
+//   }
 
 //   awsServerlessExpress.proxy(server, event, context);
 // }
+
+exports.handler = (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  connection = mongoose
+  .connect(process.env.MONGODB_URI, { family: 4 })
+  .then(console.log("Connected to Mongo!"))
+  .catch(console.error);
+
+  awsServerlessExpress.proxy(server, event, context);
+}
