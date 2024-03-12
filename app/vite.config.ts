8ccec57f3ee46b67bf61aacdebd70af412036cc7
818523 <74215559+BlakeMoran14@users.noreply.github.com>
2024-03-12 12:26:10 -0500

    added pwa

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 import path, { resolve } from "path";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "images/superalliancelogo.png"],
+  manifest: {
+    name: "SuperAlliance",
+    short_name: "SA",
+    description: "Team 7028's Most Advanced Scouting Tool",
+    icons: [
+      {
+        src: "/images/superalliancelogo.png",
+        sizes: "2000x2000",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+    ],
+    theme_color: "#000000",
+    background_color: "#000000",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
