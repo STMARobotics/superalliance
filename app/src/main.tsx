98dc2744f6c79f596fec044681bbada16f8047c3
ben <ben.sir545@outlook.com>
2025-02-28 09:59:03 -0600

    no functional changes

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -6,6 +6,7 @@ import { dark } from "@clerk/themes";
 
 //Clerk Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+console.log(PUBLISHABLE_KEY);
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
