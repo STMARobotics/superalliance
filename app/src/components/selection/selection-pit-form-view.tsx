891ff018b4aaf1a110ad613a262b7fa75994356a
finnsherman <finnsherman999@gmail.com>
2025-02-28 12:03:00 -0600

    update team cards with correct data

--- app/src/components/selection/selection-pit-form-view.tsx
+++ app/src/components/selection/selection-pit-form-view.tsx
@@ -10,46 +10,11 @@ const SelectionPitFormView = ({
     <div>
       {pitFormData ? (
         <Accordion
-                defaultValue={["strategy"]}
+                defaultValue={["mechanical"]}
                 pt={10}
                 multiple
                 variant="contained"
               >
-                <Accordion.Item value="strategy">
-                  <Accordion.Control>Strategy</Accordion.Control>
-                  <Accordion.Panel>
-                    <Group p={12}>
-                      <Textarea
-                        placeholder="Description..."
-                        label="Overall Strategy"
-                        description="What was the team's overall strategy?"
-                        autosize
-                        minRows={1}
-                        value={pitFormData?.overallStrategy}
-                        readOnly
-                      />
-        
-                      <TextInput
-                        label="April Tags"
-                        description="Did the robot use April Tags?"
-                        value={pitFormData?.aprilTags ? "Yes" : "No"}
-                        readOnly
-                      />
-        
-                      {pitFormData?.aprilTags && (
-                        <Textarea
-                          placeholder="Description..."
-                          label="April Tags Usage"
-                          description="How were April Tags used?"
-                          autosize
-                          minRows={1}
-                          value={pitFormData?.aprilTagsUse}
-                          readOnly
-                        />
-                      )}
-                    </Group>
-                  </Accordion.Panel>
-                </Accordion.Item>
                 <Accordion.Item value="mechanical">
                   <Accordion.Control>Mechanical/Electrical</Accordion.Control>
                   <Accordion.Panel>
@@ -73,50 +38,14 @@ const SelectionPitFormView = ({
                         value={pitFormData?.batterySecured}
                         readOnly
                       />
-        
-                      <TextInput
-                        label="Cameras"
-                        description="Did the robot have cameras?"
-                        value={pitFormData?.cameras ? "Yes" : "No"}
-                        readOnly
-                      />
-        
-                      {pitFormData?.cameras && (
-                        <Textarea
-                          placeholder="Description..."
-                          label="Camera Usage"
-                          description="How were the cameras used?"
-                          autosize
-                          minRows={1}
-                          value={pitFormData?.cameraUsage}
-                          readOnly
-                        />
-                      )}
-        
+
                       <Textarea
                         placeholder="Description..."
-                        label="Drivetrain Type"
-                        description="What type of drivetrain was used?"
+                        label="High Center Of Mass"
+                        description="Does the robot have a high center of mass?"
                         autosize
                         minRows={1}
-                        value={pitFormData?.drivetrainType}
-                        readOnly
-                      />
-        
-                      <Textarea
-                        placeholder="Description..."
-                        label="Drivetrain Brand"
-                        description="What brand was the drivetrain?"
-                        autosize
-                        minRows={1}
-                        value={pitFormData?.drivetrainBrand}
-                        readOnly
-                      />
-        
-                      <TextInput
-                        label="Backup Swerve"
-                        description="Did the robot have backup swerve modules?"
-                        value={pitFormData?.backupSwerve ? "Yes" : "No"}
+                        value={pitFormData?.highCenterOfMass}
                         readOnly
                       />
                     </Group>
@@ -126,72 +55,32 @@ const SelectionPitFormView = ({
                   <Accordion.Control>Driveteam/Competition</Accordion.Control>
                   <Accordion.Panel>
                     <Group p={12}>
+
                       <Textarea
-                        placeholder="Description..."
-                        label="Complimentary Robot"
-                        description="What type of robot compliments them?"
-                        autosize
-                        minRows={1}
-                        value={pitFormData?.complimentaryRobot}
+                        label="Coral Stuck"
+                        description="Can coral get stuck on your robot?"
+                        value={pitFormData?.coralStuck ? "Yes" : "No"}
                         readOnly
                       />
-        
+                      
                       <Textarea
-                        placeholder="Description..."
-                        label="Robot Changes"
-                        description="Changes since last competition/year?"
-                        autosize
-                        minRows={1}
-                        value={pitFormData?.robotChanges}
+                        label="Drive Coach Experience"
+                        description="How many competitions has the drive coach been to?"
+                        value={pitFormData?.coachExperience}
                         readOnly
                       />
-        
-                      <TextInput
-                        label="Score L1"
-                        description="Did the robot score by L1?"
-                        value={pitFormData?.scoreL1 ? "Yes" : "No"}
+
+                      <Textarea
+                        label="Driver Experience"
+                        description="How many competitions has the driver been to?"
+                        value={pitFormData?.driverExperience}
                         readOnly
                       />
-        
-                      <TextInput
-                        label="Score L2"
-                        description="Did the robot score by L2?"
-                        value={pitFormData?.scoreL2 ? "Yes" : "No"}
-                        readOnly
-                      />
-        
-                      <TextInput
-                        label="Score L3"
-                        description="Did the robot score by L3?"
-                        value={pitFormData?.scoreL3 ? "Yes" : "No"}
-                        readOnly
-                      />
-        
-                      <TextInput
-                        label="Score L4"
-                        description="Did the robot score by L4?"
-                        value={pitFormData?.scoreL4 ? "Yes" : "No"}
-                        readOnly
-                      />
-        
-                      <TextInput
-                        label="Score Processor"
-                        description="Did the robot score via the processor?"
-                        value={pitFormData?.scoreProcessor ? "Yes" : "No"}
-                        readOnly
-                      />
-        
-                      <TextInput
-                        label="Score Shallow"
-                        description="Did the robot score by climbing shallow?"
-                        value={pitFormData?.scoreShallow ? "Yes" : "No"}
-                        readOnly
-                      />
-        
-                      <TextInput
-                        label="Score Deep"
-                        description="Did the robot score by climbing deep?"
-                        value={pitFormData?.scoreDeep ? "Yes" : "No"}
+
+                      <Textarea
+                        label="Operator Experience"
+                        description="How many competitions has the operator been to?"
+                        value={pitFormData?.operatorExperience}
                         readOnly
                       />
         
@@ -228,23 +117,6 @@ const SelectionPitFormView = ({
                         />
                       )}
         
-                      <TextInput
-                        label="Auto"
-                        description="Did the robot have an auto?"
-                        value={pitFormData?.auto ? "Yes" : "No"}
-                        readOnly
-                      />
-        
-                      {pitFormData?.auto && (
-                        <TextInput
-                          placeholder="Description..."
-                          label="Auto Count"
-                          description="How many?"
-                          value={pitFormData?.autoCount}
-                          readOnly
-                        />
-                      )}
-        
                       <Textarea
                         placeholder="Description..."
                         label="Ideal Auto"
@@ -254,16 +126,7 @@ const SelectionPitFormView = ({
                         value={pitFormData?.idealAuto}
                         readOnly
                       />
-        
-                      <Textarea
-                        placeholder="Description..."
-                        label="Robot Issues"
-                        description="Issues with your robot?"
-                        autosize
-                        minRows={1}
-                        value={pitFormData?.robotIssues}
-                        readOnly
-                      />
+
                     </Group>
                   </Accordion.Panel>
                 </Accordion.Item>
@@ -278,7 +141,7 @@ const SelectionPitFormView = ({
                         value={pitFormData?.preferredDriverStation}
                         readOnly
                       />
-        
+
                       <TextInput
                         placeholder="Description..."
                         label="Preferred Human Player Placement"
@@ -314,6 +177,25 @@ const SelectionPitFormView = ({
                         autosize
                         minRows={1}
                         value={pitFormData?.extraComments}
+                        readOnly
+                      />
+
+                      <Textarea
+                        placeholder="1-10"
+                        label="Pit Rating"
+                        description="How good the pit was to scout"
+                        autosize
+                        minRows={1}
+                        value={pitFormData?.pitRating}
+                        readOnly 
+                      />
+                      <Textarea
+                        placeholder="1-5"
+                        label="Robot Rating"
+                        description="How good the robot was"
+                        autosize
+                        minRows={1}
+                        value={pitFormData?.robotRating}
                         readOnly
                       />
                     </Group>
--- app/src/components/selection/selection-team-view.tsx
+++ app/src/components/selection/selection-team-view.tsx
@@ -284,17 +284,16 @@ export const DataDisplay = ({
               <Card>
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                   <CardTitle className="text-sm font-medium">
-                    Average Auto Coral
+                    Pit Rating
                   </CardTitle>
                   <IconRobot className="h-4 w-4 text-muted-foreground" />
                 </CardHeader>
                 <CardContent>
                   <div className="text-2xl font-bold">
-                    {aggregationData?.avgAutoCoral} Coral
+                    {pitFormData?.pitRating} / 10
                   </div>
                   <p className="text-xs text-muted-foreground">
-                    {aggregationData?.totalAutoCoral} coral across{" "}
-                    {aggregationData?.matchCount} matches.
+                  Robot Rating {" "} {pitFormData?.robotRating} / 5
                   </p>
                 </CardContent>
               </Card>
